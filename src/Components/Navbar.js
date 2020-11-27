0cff7aa42cb1472ff2a9f1175b9e349038542045
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-11-27 13:05:32 +0000

    Add the JASMIN logo to the header

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import Navbar from 'react-bootstrap/Navbar';
 
+import Logo from 'fwtheme-react-jasmin/Logo';
+
 
 const ManageNavBar = () => (
     <Navbar expand="lg" variant="dark" bg="primary">
-        <Navbar.Brand>JASMIN Projects Portal</Navbar.Brand>
+        <Navbar.Brand className="d-flex align-items-center">
+            <Logo height={40} /> <span className="pt-1">Projects Portal</span>
+        </Navbar.Brand>
     </Navbar>
 );
 
