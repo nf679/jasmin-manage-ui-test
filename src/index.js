b62730a41b18ea8d677c74cbfa5917c4337cb9de
Matt Pryor <matt.pryor@stfc.ac.uk>
2021-03-01 10:05:15 +0000

    Upgrade react-bootstrap-notify to use React state/context version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { NotificationProvider } from 'react-bootstrap-notify';
+
 import JasminApp from 'fwtheme-react-jasmin';
 
 import App from './Components';
@@ -9,7 +11,9 @@ import App from './Components';
 ReactDOM.render(
     <React.StrictMode>
         <JasminApp>
-            <App />
+            <NotificationProvider>
+                <App />
+            </NotificationProvider>
         </JasminApp>
     </React.StrictMode>,
     document.getElementById('root')
