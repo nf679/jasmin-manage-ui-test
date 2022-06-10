302623a56c1a3b7fade5b90043c69f5cf968a28d
Jack Leland <jack.e.leland@gmail.com>
2022-06-10 16:21:36 +0100

    Further stripping to fix 404

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -4,33 +4,31 @@ import { LinkContainer } from 'react-router-bootstrap';
 import Nav from 'react-bootstrap/Nav'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Alert from 'react-bootstrap/Alert'
+import Alert from 'react-bootstrap/Alert'
 
 class NotFoundPage extends React.Component{
     render(){
         return (
-            <Row>
-                <Col>
-                    <h1>Page Not Found</h1>
-                        {/* <div className="alert alert-with-icon alert-warning">
-                            <i className="fas fa-exclamation-circle"></i> <span>The page you are looking for does not exist</span>
-                        </div> */}
-                    {/* <Alert variant={warning}>
-                        The page you are looking for does not exist
-                    </Alert> */}
+            <div>
+                <h1>Page Not Found</h1>
+                {/* <div className="alert alert-with-icon alert-warning">
+                    <i className="fas fa-exclamation-circle"></i> <span>The page you are looking for does not exist</span>
+                </div> */}
+                <Alert variant={warning}>
+                    The page you are looking for does not exist
+                </Alert>
 
-                    <p>The page you requested does not exist on this server.</p>
-                    <p>
-                    You could try:
-                    </p>
-                    <LinkContainer to="/">
-                        <Nav.Link>Return to homepage</Nav.Link> 
-                    </LinkContainer>
-                    <LinkContainer to="/projects">
-                        <Nav.Link>Return to your projects</Nav.Link>
-                    </LinkContainer>
-                </Col>
-            </Row>
+                <p>The page you requested does not exist on this server.</p>
+                <p>
+                You could try:
+                </p>
+                <LinkContainer to="/">
+                    <Nav.Link>Return to homepage</Nav.Link> 
+                </LinkContainer>
+                <LinkContainer to="/projects">
+                    <Nav.Link>Return to your projects</Nav.Link>
+                </LinkContainer>
+            </div>
         );
     }
 }
