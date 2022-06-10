9d32eab83ab07ff4888b569586a3bb4b7cc765e4
Jack Leland <jack.e.leland@gmail.com>
2022-06-10 13:21:34 +0100

    Visual upgrade to 404 page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -2,23 +2,37 @@ import React from 'react';
 
 import { LinkContainer } from 'react-router-bootstrap';
 import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert'
 
 class NotFoundPage extends React.Component{
     render(){
         return (
-            <div>
-                {/* <div style={{class:"alert alert-with-icon alert-warning", role:"alert"}}>
-                    <i style={{class:"fas fa-exclamation-circle"}}></i> <span>The page you are looking for does not exist</span>
-                </div> */}
-                <p>The page you requested does not exist on this server.</p>
-        
-                <LinkContainer to="/">
-                    <Nav.Link>Return to homepage</Nav.Link> 
-                </LinkContainer>
-                <LinkContainer to="/projects">
-                    <Nav.Link>Return to your projects</Nav.Link>
-                </LinkContainer>
-            </div>
+            <Row>
+                <Col>
+                    <h1>Page Not Found</h1>
+                    <div>
+                        <div className="alert alert-with-icon alert-warning">
+                            <i className="fas fa-exclamation-circle"></i> <span>The page you are looking for does not exist</span>
+                        </div>
+                        <Alert variant={warning}>
+                            The page you are looking for does not exist
+                        </Alert>
+
+                        <p>The page you requested does not exist on this server.</p>
+                        <p>
+                        You could try:
+                        <LinkContainer to="/">
+                            <Nav.Link>Return to homepage</Nav.Link> 
+                        </LinkContainer>
+                        <LinkContainer to="/projects">
+                            <Nav.Link>Return to your projects</Nav.Link>
+                        </LinkContainer>
+                        </p>
+                    </div>
+                </Col>
+            </Row>
         );
     }
 }
