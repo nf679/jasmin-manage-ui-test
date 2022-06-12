103d2dc3608ac8ab8dbcd044eaca5f03a412a48b
Jack Leland <jack.e.leland@gmail.com>
2022-06-12 11:23:30 +0100

    Add row and col containers

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -9,26 +9,25 @@ import Alert from 'react-bootstrap/Alert';
 class NotFoundPage extends React.Component{
     render(){
         return (
-            <div>
-                <h1>Page Not Found</h1>
-                {/* <div className="alert alert-with-icon alert-warning">
-                    <i className="fas fa-exclamation-circle"></i> <span>The page you are looking for does not exist</span>
-                </div> */}
-                <Alert variant='warning'>
-                    <i className="fas fa-exclamation-circle"></i>The page you are looking for does not exist
-                </Alert>
+            <Row>
+                <Col>
+                    <h1>Page Not Found</h1>
+                    <Alert variant='warning'>
+                        <i className="fas fa-exclamation-circle"></i> The page you are looking for does not exist
+                    </Alert>
 
-                <p>The page you requested does not exist on this server.</p>
-                <p>
-                You could try:
-                </p>
-                <LinkContainer to="/">
-                    <Nav.Link>Return to homepage</Nav.Link> 
-                </LinkContainer>
-                <LinkContainer to="/projects">
-                    <Nav.Link>Return to your projects</Nav.Link>
-                </LinkContainer>
-            </div>
+                    <p>The page you requested does not exist on this server.</p>
+                    <p>
+                    You could try:
+                    </p>
+                    <LinkContainer to="/">
+                        <Nav.Link>Return to homepage</Nav.Link> 
+                    </LinkContainer>
+                    <LinkContainer to="/projects">
+                        <Nav.Link>Return to your projects</Nav.Link>
+                    </LinkContainer>
+                </Col>
+            </Row>
         );
     }
 }
