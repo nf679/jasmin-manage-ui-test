fab4b2f81072d2456a60d0d423bd6a76fc98031b
nf679 <nicola.farmer@stfc.ac.uk>
2023-06-16 11:45:56 +0100

    Remove jumbotron (not available in new bootstrap)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 
 import { LinkContainer } from 'react-router-bootstrap';
@@ -11,14 +10,14 @@ import { LinkContainer } from 'react-router-bootstrap';
 const Home = () => (
     <Row>
         <Col>
-            <Jumbotron>
+            <div class="jumbotron">
                 <h1>JASMIN Projects Portal</h1>
                 <p>Welcome to the JASMIN Projects Portal.</p>
                 <p>Using this portal, you can request JASMIN resources for your project and track their status.</p>
                 <LinkContainer to="/projects">
                     <Button variant="primary" size="lg">Manage my projects</Button>
                 </LinkContainer>
-            </Jumbotron>
+            </div>
         </Col>
     </Row>
 );
