d271dfbecd16b1885372118d069781a766937bb6
Matthew Jones <matthew.j.jones@stfc.ac.uk>
2024-04-18 11:08:36 +0100

    table order should be correct, and sorting enabled

--- src/Components/Consortium/ProjectSummaryPane.js
+++ src/Components/Consortium/ProjectSummaryPane.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
+import { useState } from "react";
+
+import classNames from 'classnames';
 
 import { useHistory, useLocation } from 'react-router-dom';
 
@@ -21,12 +23,66 @@ import { SpinnerWithText, formatAmount, sortByKey } from '../utils';
 
 import '../../css/quota-card.css';
 
+const TableHead = ({ columns, handleSorting }) => {
+    const [sortField, setSortField] = useState("");
+    const [order, setOrder] = useState("asc");
+    const handleSortingChange = (accessor) => {
+        const sortOrder =
+            accessor === sortField && order === "asc" ? "desc" : "asc";
+        setSortField(accessor);
+        setOrder(sortOrder);
+        handleSorting(accessor, sortOrder);
+    };
+    return (
+        <>
+            <thead>
+                <tr>
+                    {columns.map(({ label, accessor, sortable }) => {
+                        const cl = sortable
+                            ? sortField === accessor && order === "asc"
+                                ? "(asc)"
+                                : sortField === accessor && order === "desc"
+                                    ? "(desc)"
+                                    : ""
+                            : "";
+                        return <th
+                            key={accessor}
+                            onClick={sortable ? () => handleSortingChange(accessor) : null}
+                        >
+                            {label + ' ' + cl}
+                        </th>;
+                    })}
+                </tr>
+            </thead>
+        </>
+    )
+}
+
+const TableBody = ({ tableData, columns }) => {
+    return (
+        <>
+            <tbody>
+                {tableData.map((data) => {
+                    return (
+                        <tr key={data.id}>
+                            {columns.map(({ accessor }) => {
+                                const tData = data[accessor] ? data[accessor] : "—";
+                                return <td key={accessor}>{tData}</td>;
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </>
+    )
+}
 
 const SummaryPane = ({ conSummary }) => {
     // If there's an error, push to the main consortia page
     const history = useHistory();
     const location = useLocation();
     const handleError = error => history.push(location.pathname.replace('/summary', ''));
+
 
     // const resources = useResources();
     // useEnsureInitialised(resources);
@@ -40,7 +96,75 @@ const SummaryPane = ({ conSummary }) => {
     // const sortedHeaders = resheaders.sort();
 
     // Define columns
+    const columnsInput = Object.keys(conSummary.data.project_summaries[0].resource_summary)
+    var columns = [{ label: 'project', accessor: 'project', sortable: true }, { label: 'tags', accessor: 'tags', sortable: true }, { label: 'collaborators', accessor: 'collaborators', sortable: true }];
+    var i = 0;
+    while (i < Object.values(columnsInput).length) {
+        columns = [...columns, { label: Object.values(columnsInput)[i], accessor: Object.values(columnsInput)[i].replaceAll(' ', '_'), sortable: true }];
+        i++
+    }
 
+    var tableData1 = [];
+    var i = 0;
+    // Create table data
+    while (i < Object.keys(conSummary.data.project_summaries).length) {
+        var dataline = {};
+        dataline['project'] = conSummary.data.project_summaries[i]['project_name']
+        var t = 0;
+        var tags = [];
+        while (t < conSummary.data.project_summaries[i]['tags'].length) {
+            tags = [...tags, conSummary.data.project_summaries[i]['tags'][t] + ', ']
+            t++;
+        }
+        if (tags.length >= 1) {
+            tags[t - 1] = tags[t - 1].slice(0, -2)
+        }
+        dataline['tags'] = tags
+        var collabs = [];
+        var c = 0;
+        while (c < Object.values(conSummary.data.project_summaries[i]['collaborators']).length) {
+            collabs = [...collabs, conSummary.data.project_summaries[i]['collaborators'][c]['username'] + ', '];
+            c++
+        }
+        collabs[c - 1] = collabs[c - 1].slice(0, -2)
+
+        dataline['collaborators'] = collabs;
+        tableData1 = [...tableData1, dataline]
+        var r = 0;
+        while (r < Object.values(conSummary.data.project_summaries[i]['resource_summary']).length) {
+            dataline[Object.keys(conSummary.data.project_summaries[i]['resource_summary'])[r].replaceAll(' ', '_')] = Object.values(conSummary.data.project_summaries[i]['resource_summary'])[r]
+
+            r++
+        }
+        i++
+    }
+
+    // Handle the sorting of data
+    const [tableData, setTableData] = useState(tableData1);
+
+    const handleSortingChange = (accessor) => {
+        const sortOrder =
+            accessor === sortField && order === "asc" ? "desc" : "asc";
+        setSortField(accessor);
+        setOrder(sortOrder);
+        handleSorting(accessor, sortOrder);
+    };
+
+    const handleSorting = (sortField, sortOrder) => {
+        if (sortField) {
+            const sorted = [...tableData].sort((a, b) => {
+                if (a[sortField] === null) return 1;
+                if (b[sortField] === null) return -1;
+                if (a[sortField] === null && b[sortField] === null) return 0;
+                return (
+                    a[sortField].toString().localeCompare(b[sortField].toString(), "en", {
+                        numeric: true,
+                    }) * (sortOrder === "asc" ? 1 : -1)
+                );
+            });
+            setTableData(sorted);
+        }
+    };
 
     return (
         <Status fetchable={conSummary}>
@@ -55,32 +179,17 @@ const SummaryPane = ({ conSummary }) => {
             <Status.Available>
                 <>
                     <Col>
-                        <Table striped responsive="md" size='sm'>
-                            <thead>
-                                <tr>
-                                    <th>Project</th>
-                                    <th>Tags</th>
-                                    <th>Collaborators</th>
-                                    {/* {sortedHeaders.map(res => (<th>{res}</th>))
-                                    } */}
-                                    {Object.keys(conSummary.data.project_summaries[0].resource_summary).map(res => (<th>{res}</th>))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {Object.values(conSummary.data.project_summaries).map(project => (
-                                    <tr>
-                                        <td>{project.project_name}</td>
-                                        <td><>{Object.values(project.tags).map(tag => (<Row><Col>{tag}</Col></Row>))}</></td>
-                                        <td>{Object.values(project.collaborators).map(collab => (<Row><Col>{collab.username}</Col></Row>))}</td>
-                                        {Object.keys(project.resource_summary).map(res => (<td>{res}</td>))}
-                                    </tr>
-                                ))}
-                            </tbody>
+                        <header>Resource summaries for all projects in Consortia. Column headers can be clicked on to order them.</header>
+                        <Table striped responsive="md" size='sm' >
+
+
+                            <TableHead columns={columns} handleSorting={handleSorting} />
+                            <TableBody tableData={tableData} columns={columns} />
                         </Table>
                     </Col>
                 </>
             </Status.Available>
-        </Status>);
+        </Status >);
 };
 
 
